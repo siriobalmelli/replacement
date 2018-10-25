a86ac3286fe599271b596aa903cb88011ee6a750
Sirio Balmelli <siriobalmelli@MacBook-Pro-di-Sirio.local>
2018-10-25 17:47:18 +0200

    working recursion

--- replacement.py
+++ replacement.py
@@ -12,16 +12,13 @@ import string
 import ruamel.yaml
 
 
-def subst_dict(dic={}, meta={}, method='literal'):  # pylint: disable=dangerous-default-value
+def subst_dict(dic, meta, method):  # pylint: disable=dangerous-default-value
     '''subst_dict()
     String-coerce all string-able values in 'dic', optionally substitute them
     against 'meta' using 'method'.
     Does NOT recurse into sub-objects or lists.
     '''
-    print('--- subst_dict: ---')
-    print(dic)
-    print(meta)
-    print('-------------------')
+    dic = dic or {}
     methods = {'literal': lambda stg, dic: stg,
                'format': lambda stg, dic: stg.format(**dic),
                'substitute': lambda stg, dic: string.Template(stg).substitute(**dic),
@@ -31,11 +28,12 @@ def subst_dict(dic={}, meta={}, method='literal'):  # pylint: disable=dangerous-
     return {k: sub(str(v), meta) if isinstance(v, (str, float, int)) else v
             for k, v in dic.items()}
 
-def subst_line(lin=[], meta={}, method='literal'):  # pylint: disable=dangerous-default-value
+def subst_line(lin, meta, method):
     '''subst_line()
     Clean up or substitute each text line according to 'method'.
     NOTE: 'meta' must contain an "eol" entry.
     '''
+    lin = lin or []
     methods = {'literal': lambda stg, dic: stg,
                'format': lambda stg, dic: stg.format(**dic),
                'substitute': lambda stg, dic: string.Template(stg).substitute(**dic),
@@ -45,10 +43,11 @@ def subst_line(lin=[], meta={}, method='literal'):  # pylint: disable=dangerous-
     return [sub(lin, meta).rstrip(meta['eol']) for lin in lin]
 
 
-def merge_dict(in_to, *out_of):  # pylint: disable=dangerous-default-value
+def merge_dict(in_to, *out_of):
     ''''merge_dict()
     Merge each dictionary in 'out_of' into 'in_to' and return it.
     '''
+    in_to = in_to or {}
     for obj in out_of:
         def scalar(thing=None):
             return thing is None or isinstance(thing, str) or not hasattr(thing, "__len__")
@@ -98,11 +97,14 @@ def merge_dict(in_to, *out_of):  # pylint: disable=dangerous-default-value
         # garbage!
         else:
             raise Exception("cannot merge type '{0}'".format(type(obj)))
+    return in_to
 
-def merge_line(in_to=[], out_of=[]):  # pylint: disable=dangerous-default-value
+def merge_line(in_to, out_of):
     '''merge_line()
     Concatenate 2 lists of lines
     '''
+    in_to = in_to or []
+    out_of = out_of or []
     return in_to + out_of
 
 
@@ -114,29 +116,48 @@ def get_file(path):
         return [lin for lin in fil]
 
 
-def do_block(blk={}, meta={}):  # pylint: disable=dangerous-default-value
+def render_string(lst, meta):
+    '''render_string()
+    stringify a list of lines
+    '''
+    return meta['eol'].join(lst)
+
+
+def do_block(blk, meta):
     ''''do_block()
     Process a block.
     Recursive.
     Returns ('out', 'meta').
     '''
+    blk = blk or {}
     # Preprocess block before parsing it.
     # Will string-coerce scalars in the block whether a preprocessing directive
     # was given or not.
     blk = subst_dict(blk, meta, blk.get('prep'))
 
-    yields = {'text': lambda output: (subst_line(output, meta, blk.get('proc')),
-                                      meta),
-              'dict': lambda output: (subst_dict(output, meta, blk.get('proc')),
-                                      meta),
-              'meta': lambda output: (None,
-                                      merge_dict(meta,
-                                                 subst_dict(output, meta, blk.get('proc'))))
+    def flatten(value):
+        '''flatten()
+        '''
+        if isinstance(value, list) and len(value) is 1:
+            value = value[0]
+        return value
+
+    # 'im' is "intermediate" (denoting it should be run through normalize() below
+    yields = {'text': lambda im: (subst_line(im, meta, blk.get('proc')),
+                                  meta),
+              'dict': lambda im: (subst_dict(im, meta, blk.get('proc')),
+                                  meta),
+              'meta': lambda im: (None,
+                                  # merge into 'meta' at 'spec' key (clobber meta)
+                                  merge_dict(subst_dict({blk['spec']: flatten(im)},
+                                                        meta,
+                                                        blk.get('proc')),
+                                             meta))
              }
 
     inp = blk['input']  # it is a hard fault not to have 'input'
     if isinstance(inp, str):  # relies on string coercion in "preprocess" above
-        inputs = {'text': lambda: [lin for lin in inp.split(meta['eol'])],
+        inputs = {'text': lambda: [lin for lin in inp.strip(meta['eol']).split(meta['eol'])],
                   'dict': None,
                   'meta': None,
                   'file': lambda: get_file(inp),
@@ -159,13 +180,13 @@ def do_block(blk={}, meta={}):  # pylint: disable=dangerous-default-value
     do_yield = yields[do_yield]
     do_input = inputs[do_input]
 
-    print('--- do_block: ---')
-    print(blk)
-    print(meta)
-    print(inp)
+    #print('--- do_block: ---')
+    #print(blk)
+    #print(meta)
+    #print(inp)
     ret = do_yield(do_input())
-    print(ret)
-    print('-----------------')
+    #print(ret)
+    #print('-----------------')
     return ret
 
 def do_recurse(blk_list=[], meta={}, merge_func=merge_line):  # pylint: disable=dangerous-default-value
@@ -175,8 +196,9 @@ def do_recurse(blk_list=[], meta={}, merge_func=merge_line):  # pylint: disable=
     - propagating any changes to 'meta' through to successive blocks
 
     NOTE that we do not, ourselves, return a (potentially modified) 'meta'.
+    NOTE that merge_func() MUST be able to accept a None input.
     '''
-    out = merge_func()  # should return an empty object of the correct type
+    out = None
     for blk in blk_list:
         data, meta = do_block(blk, meta)
         if data:  # may have been a 'meta' block returning no data
@@ -184,7 +206,7 @@ def do_recurse(blk_list=[], meta={}, merge_func=merge_line):  # pylint: disable=
     return out
 
 
-def replacement(path={}, meta={}):  # pylint: disable=dangerous-default-value
+def replacement(path, meta):
     '''replacement()
     path    :   path to the YAML template
                 to be opened and processed
@@ -205,14 +227,15 @@ def replacement(path={}, meta={}):  # pylint: disable=dangerous-default-value
         os.chdir(chd)
 
     # sane default for line endings
+    meta = meta or {}
     if 'eol' not in meta:
         meta['eol'] = '\n'
 
-    out, meta = [line for line in do_recurse(template, meta)]
+    out = [line for line in do_recurse(template, meta)]
 
     if chd:
         os.chdir(cwd)
-    return meta['eol'].join(out)
+    return render_string(out, meta)
 
 
 def main():
