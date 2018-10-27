022427e21f6f35f88feb440983472b54fd733ec4
Sirio Balmelli <sirio@b-ad.ch>
2018-10-27 13:18:15 +0200

    lint setup

--- setup.py
+++ setup.py
@@ -1,22 +1,28 @@
-# Setup.py
-import setuptools
+'''setup.py
+PyPI build for replacement package
+(c) 2018 Anthony Soenen and Sirio Balmelli
+'''
+from setuptools import setup, find_packages
 
 with open("README.md", "r") as fh:
-    long_description = fh.read()
+    LONG = fh.read()
 
-setuptools.setup(
+setup(
     name="replacement",
-    version="0.2rc1",
-    author="Sirio Balmelli",
-    author_email="sirio@b-ad.ch",
-    description="Replacement is a python utility that parses a yaml template and outputs text.",
-    license='Apache',
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/siriobalmelli/replacement",
-    packages=setuptools.find_packages(),
+    version="0.2rc2",
+    packages=find_packages(),
+
     install_requires=['ruamel.yaml'],
     python_requires='>=3.5',
+
+    author="Sirio Balmelli",
+    author_email="sirio.bm@gmail.com",
+    description="Replacement is a python utility that parses a yaml template and outputs text.",
+    license='Apache',
+    keywords='replacement template substitute compile',
+    url="https://github.com/siriobalmelli/replacement",
+    long_description=LONG,
+    long_description_content_type="text/markdown",
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: Apache Software License",
