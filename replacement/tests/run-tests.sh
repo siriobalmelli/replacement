2ddc795f8e375a9ae2d8e6c606e4964ad7312a7e
Sirio Balmelli <sirio@b-ad.ch>
2018-10-28 12:28:24 +0100

    give status on test runs

--- replacement/tests/run-tests.sh
+++ replacement/tests/run-tests.sh
@@ -1,13 +1,29 @@
 #!/bin/bash
 
-set -e
-
 pushd $(dirname $(readlink $0))
+
+FAIL=""
+PASS=""
 
 for a in *.yaml; do
 	yamllint "$a"
-	diff <(../replacement.py -t "$a") "${a%.yaml}.out"
-	echo "$a"
+	if ../replacement.py -t "$a" >"${a%.yaml}.temp" \
+		&& diff "${a%.yaml}.temp" "${a%.yaml}.out"
+	then
+		PASS="${PASS}$a\n"
+	else
+		FAIL="${FAIL}$a\n"
+	fi
+	rm -f "${a%.yaml}.temp"
 done
+
+# flush current stdout (if user is watching all logs)
+echo -e "\nPASS:\n$PASS" >&2
+
+if [[ $FAIL ]]; then
+	echo -e "FAIL:\n$FAIL" >&2
+	exit 1
+fi
+exit 0
 
 popd
